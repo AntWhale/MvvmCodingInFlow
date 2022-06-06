be6b192f7e932b2dda68581cd7f58a2b7c70eaf6
Boo <Antwhale94@gmail.com>
2022-06-07 01:03:41 +0900

    Create OkHttpLoggingInterceptor in hilt

--- app/src/main/java/com/codinginflow/mvvm/di/AppModule.kt
+++ app/src/main/java/com/codinginflow/mvvm/di/AppModule.kt
@@ -6,8 +6,11 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -15,14 +18,33 @@ import javax.inject.Singleton
 object AppModule {
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit =
+    fun provideRetrofit(client: OkHttpClient): Retrofit =
         Retrofit.Builder()
             //.baseUrl(UnsplashApi.BASE_URL)
             .baseUrl(Config.INSTANCE.apiUrl)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
     @Provides
     @Singleton
     fun provideUnsplashApi(retrofit: Retrofit): UnsplashApi = retrofit.create(UnsplashApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideClient(): OkHttpClient {
+        val client = OkHttpClient.Builder()
+            .connectTimeout(Config.INSTANCE.connectTimeOut, TimeUnit.MILLISECONDS)
+            .readTimeout(Config.INSTANCE.readTimeOut, TimeUnit.MILLISECONDS)
+            .writeTimeout(Config.INSTANCE.writeTimeOut, TimeUnit.MILLISECONDS)
+
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        return client
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+
 }
